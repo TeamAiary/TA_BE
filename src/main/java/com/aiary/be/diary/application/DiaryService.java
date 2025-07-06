f69d7d5529d50112593ee3ef12b2f991e1f4948a
BaeJunH0 <bjh14757@naver.com>
2025-07-06 16:00:37 +0900

    feat : 하루에 여러 개의 일기를 쓸 수 없도록 제한

--- src/main/java/com/aiary/be/diary/application/DiaryService.java
+++ src/main/java/com/aiary/be/diary/application/DiaryService.java
@@ -13,8 +13,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -28,6 +31,13 @@ public class DiaryService {
     }
     
     @Transactional(readOnly = true)
+    public List<DiaryInfo> readDiaryInfos(Long userId, LocalDateTime[] range) {
+        return diaryRepository.findAllByUserIdAndCreatedAtBetween(userId, range[0], range[1]).stream()
+                   .map(DiaryInfo::from)
+                   .toList();
+    }
+    
+    @Transactional(readOnly = true)
     public DiaryInfo readDiaryInfo(Long diaryId) {
         Diary diary = diaryRepository.findById(diaryId).orElseThrow(
             () -> CustomException.from(DiaryErrorCode.NOT_FOUND)
@@ -38,6 +48,18 @@ public class DiaryService {
     
     @Transactional
     public void createDiary(User user, DiaryRequest diaryRequest) {
+        Optional<Diary> exist = diaryRepository.findByUserIdOrderByIdDesc(user.getId());
+        
+        if(exist.isPresent()) {
+            int year = exist.get().getCreatedAt().getYear();
+            int month = exist.get().getCreatedAt().getMonthValue();
+            int day = exist.get().getCreatedAt().getDayOfMonth();
+            
+            if(LocalDate.now().isEqual(LocalDate.of(year, month, day))) {
+                throw CustomException.from(DiaryErrorCode.ALREADY_EXIST_DAY);
+            }
+        }
+        
         Diary diary = new Diary(
             user,
             diaryRequest.title(), diaryRequest.content(),
--- src/main/java/com/aiary/be/diary/persistent/DiaryRepository.java
+++ src/main/java/com/aiary/be/diary/persistent/DiaryRepository.java
@@ -6,9 +6,17 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
     Page<Diary> findAllByUserIdAndCreatedAtBetween(
         Long userId, LocalDateTime start, LocalDateTime end, Pageable pageable
     );
+    
+    List<Diary> findAllByUserIdAndCreatedAtBetween(
+        Long userId, LocalDateTime start, LocalDateTime end
+    );
+    
+    Optional<Diary> findByUserIdOrderByIdDesc(Long userId);
 }
