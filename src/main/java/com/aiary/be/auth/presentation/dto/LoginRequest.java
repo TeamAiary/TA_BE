f91d8b5cf86c231125d70f5f2e2cbc1ccb9f9e15
Hyun <hyun_7586@naver.com>
2025-07-07 18:30:35 +0900

    chore: LoginRequest 사용하지 않는 import문 제거

--- src/main/java/com/aiary/be/auth/presentation/dto/LoginRequest.java
+++ src/main/java/com/aiary/be/auth/presentation/dto/LoginRequest.java
@@ -1,11 +1,5 @@
 package com.aiary.be.auth.presentation.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 public record LoginRequest(
     String email,
     String password
