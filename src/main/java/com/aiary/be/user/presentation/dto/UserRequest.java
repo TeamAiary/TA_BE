821bc187a3680cdafe1356478fa3762fe51ef82f
Hyun <hyun_7586@naver.com>
2025-07-11 01:00:49 +0900

    chore: 주석 추가

--- src/main/java/com/aiary/be/user/presentation/dto/UserRequest.java
+++ src/main/java/com/aiary/be/user/presentation/dto/UserRequest.java
@@ -2,8 +2,9 @@ package com.aiary.be.user.presentation.dto;
 
 import com.aiary.be.user.domain.Gender;
 
-// todo: valid 추가
-
+// user 정보 update에만 사용되는 dto
+// 나중에 login/signup request 등이랑 통합 예정
+// 얘는 전달하지 않는 필드는 update하지 않기 때문에 valid를 추가하지 않았음
 public record UserRequest(
     String email,
     String password,
