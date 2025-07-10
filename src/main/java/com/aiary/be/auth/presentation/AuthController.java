944767e3ee2c709c97de1fc8970283becba7bb9c
Hyun <hyun_7586@naver.com>
2025-07-11 01:00:39 +0900

    refactor: Login/Register 유효성 검증 로직 추가(email, password, username)

--- src/main/java/com/aiary/be/auth/presentation/AuthController.java
+++ src/main/java/com/aiary/be/auth/presentation/AuthController.java
@@ -7,6 +7,7 @@ import com.aiary.be.user.presentation.dto.UserResponse;
 import com.aiary.be.global.response.Message;
 import com.aiary.be.global.util.SessionUtil;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,7 +27,7 @@ public class AuthController {
     // 회원가입
     @PostMapping("/register")
     public ResponseEntity<?> register(
-        @RequestBody SignupRequest signupRequest
+        @Valid @RequestBody SignupRequest signupRequest
     ) {
         authService.save(signupRequest);
 
@@ -37,7 +38,7 @@ public class AuthController {
     // 로그인
     @PostMapping("/login")
     public ResponseEntity<?> login(
-        @RequestBody LoginRequest loginRequest,
+        @Valid @RequestBody LoginRequest loginRequest,
         BindingResult bindingResult,
         HttpServletRequest httpServletRequest
     ) {
--- src/main/java/com/aiary/be/auth/presentation/dto/LoginRequest.java
+++ src/main/java/com/aiary/be/auth/presentation/dto/LoginRequest.java
@@ -1,6 +1,15 @@
 package com.aiary.be.auth.presentation.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record LoginRequest(
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    @NotBlank(message = "이메일은 필수값입니다.")
     String email,
+
+    @NotBlank(message = "비밀번호는 필수값입니다.")
     String password
-){};
+) {
+
+};
