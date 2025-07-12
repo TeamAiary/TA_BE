325846a48af345f3d82653c2232852da59f57efe
BaeJunH0 <bjh14757@naver.com>
2025-07-13 01:14:12 +0900

    fix : 내림차순 -> 오름차순

--- src/main/java/com/aiary/be/diary/persistent/DiaryRepository.java
+++ src/main/java/com/aiary/be/diary/persistent/DiaryRepository.java
@@ -28,7 +28,7 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
         @Param("endDate") LocalDateTime end
     );
     
-    @Query("SELECT e from Diary e WHERE e.id = :userId ORDER BY e.id LIMIT 1")
+    @Query("SELECT e from Diary e WHERE e.id = :userId ORDER BY e.id DESC LIMIT 1")
     Optional<Diary> findByUserIdOrderByIdDesc(
         @Param("userId") Long userId
     );
