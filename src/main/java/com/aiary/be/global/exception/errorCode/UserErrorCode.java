1266eb47724a893a1f3583f0160760a8009142d8
Hyun <hyun_7586@naver.com>
2025-07-12 10:24:44 +0900

    refactor: 로그인이 필요한 경우에도 ProblemDetail을 통해 에러 메세지를 반환하도록 수정

--- src/main/java/com/aiary/be/global/exception/errorCode/UserErrorCode.java
+++ src/main/java/com/aiary/be/global/exception/errorCode/UserErrorCode.java
@@ -1,12 +1,14 @@
 package com.aiary.be.global.exception.errorCode;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties.Http;
 import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
     NOT_FOUND(HttpStatus.NOT_FOUND, "U001", "존재하지 않는 유저 아이디입니다."),
-    INVALID_EMAIL_PASSWORD(HttpStatus.UNAUTHORIZED, "U002", "잘못된 이메일, 비밀번호 조합입니다.");
+    INVALID_EMAIL_PASSWORD(HttpStatus.UNAUTHORIZED, "U002", "잘못된 이메일, 비밀번호 조합입니다."),
+    REQUIRED_LOGIN(HttpStatus.UNAUTHORIZED, "U003", "로그인이 필요합니다.");
     
     private final HttpStatus httpStatus;
     private final String code;
--- src/main/java/com/aiary/be/global/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/aiary/be/global/interceptor/LoginCheckInterceptor.java
@@ -1,6 +1,8 @@
 package com.aiary.be.global.interceptor;
 
 
+import com.aiary.be.global.exception.CustomException;
+import com.aiary.be.global.exception.errorCode.UserErrorCode;
 import com.aiary.be.user.presentation.dto.UserResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -24,22 +26,10 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
 
         if(session==null || session.getAttribute("loggedInUser")==null){
             log.warn("인증되지 않은 사용자, url: {}", request.getRequestURI());
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.setContentType("application/json;charset=UTF-8");
-            response.getWriter().write("{\"message\": \"로그인이 필요합니다.\"}");
 
-            return false;
+            throw CustomException.from(UserErrorCode.REQUIRED_LOGIN);
+
         }
-
-        // 이후 다른 페이지 인가 구현 예정
-//        if(request.getRequestURI().startsWith("/api/admin") &&
-//        !session.getAttribute("userRole").equals("ADMIN")){
-//            log.warn("일반 사용자는 이용할 수 없습니다.");
-//            ...
-//        }
-
-        // controller에서 자주 사용할 만한 것들은 @RequestAttribute()로 받아서 사용할 수 있도록 전달할 수 있다.
-//        request.setAttribute("userEmail", session.getAttribute("userEmail"));
         UserResponse userResponse = (UserResponse) session.getAttribute("loggedInUser");
         request.setAttribute("userId", userResponse.userId());
         return true;
