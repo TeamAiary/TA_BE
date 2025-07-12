87d103e37ba5aba3fdc94810930b935ee11307c9
BaeJunH0 <bjh14757@naver.com>
2025-07-13 01:16:51 +0900

    feat : 어드민 페이지 엔드포인트 추가

--- src/main/java/com/aiary/be/global/config/WebConfig.java
+++ src/main/java/com/aiary/be/global/config/WebConfig.java
@@ -3,11 +3,8 @@ package com.aiary.be.global.config;
 import com.aiary.be.global.interceptor.LoginCheckInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -15,11 +12,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginCheckInterceptor())
-            .addPathPatterns("/api/**")
+            .addPathPatterns(
+                "/api/**",
+                "/admin/**"
+            )
             .excludePathPatterns(
                 "/api/auth/register",
                 "/api/auth/login",
                 "/api/auth/logout",
+                "/admin",
                 "/error",
                 "/favicon.ico"
             );
